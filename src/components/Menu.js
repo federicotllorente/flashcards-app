9ab1b0c3c29a0e69a62f100a5a9a87eff86a8629
Federico Tejedor Llorente <federicotllorente@gmail.com>
2021-03-17 12:40:37 +0100

    Main menu styles finished

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import CloseMenuButton from '../img/Close Burger Menu Button.png';
 import Logo from '../img/Logo light.png';
 
 const Menu = () => {
+    const menuItems = [
+        { id: 1, name: 'Dashboard', path: '/' },
+        { id: 2, name: 'Flashcards', path: '/flashcards' },
+        { id: 3, name: 'Practice', path: '/practice' },
+        { id: 4, name: 'Settings', path: '/settings' },
+        { id: 5, name: 'Contact', path: '/contact' }
+    ];
+    const currentPage = useLocation();
     return (
         <div className="menu">
             <div className="menu__header">
@@ -14,11 +22,13 @@ const Menu = () => {
                 <img src={Logo} alt="Logo" />
             </div>
             <div className="menu__links">
-                <Link to="/">Dashboard</Link>
-                <Link to="/flashcards">Flashcards</Link>
-                <Link to="/practice">Practice</Link>
-                <Link to="/settings">Settings</Link>
-                <Link to="/contact">Contact</Link>
+                {menuItems.map(el =>
+                    (currentPage.pathname === el.path) ? (
+                        <Link key={el.id} className="currentPage" to={el.path}>{el.name}</Link>
+                    ) : (
+                        <Link key={el.id} to={el.path}>{el.name}</Link>
+                    )
+                )}
             </div>
             <div className="menu__footer">
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis quasi obcaecati nisi ut provident, reprehenderit porro praesentium sunt? Nostrum, eum.</p>
