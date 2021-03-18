40155146ffc8e0f3175a3cbd6ae9ddf73762ea42
Federico Tejedor Llorente <federicotllorente@gmail.com>
2021-03-18 13:20:31 +0100

    Category list views created. Also, I re-organized the styles and created mixins

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Categories = () => {
+    const categoryList = [
+        { id: 1, name: 'Category name 1', path: '/cat1', cards: 3 },
+        { id: 2, name: 'Category name 2', path: '/cat2', cards: 5 },
+        { id: 3, name: 'Category name 3', path: '/cat3', cards: 0 },
+        { id: 4, name: 'Category name 4', path: '/cat4', cards: 1 }
+    ];
     return (
-        <h1>Category List</h1>
+        <div className="category">
+            <h1>This is your flashcard collection</h1>
+            <div className="category__header">
+                <h2>Categories</h2>
+                <button>Create a new category</button>
+            </div>
+            <div className="category__list">
+                {categoryList.map(el => (
+                    <div key={el.id} className="category__list__item">
+                        <Link to={'/flashcards' + el.path}>
+                            <h3>{el.name}</h3>
+                            {(el.cards === 0) ? (
+                                <span>{el.cards}<span> cards yet</span></span>
+                            ) : (el.cards === 1) ? (
+                                <span>{el.cards}<span> card in total</span></span>
+                            ) : (
+                                <span>{el.cards}<span> cards in total</span></span>
+                            )}
+                        </Link>
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,49 +1,11 @@
 import React from 'react';
 
+import DashboardHome from '../views/DashboardHome';
+
 const Home = () => {
     return (
-        <div className="homepage">
-            <h1>Welcome back, FirstName!</h1>
-            <div className="homepage__reminder">
-                <h2>Reminder</h2>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum, incidunt?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, quisquam?</p>
-            </div>
-            <h2>Your statistics this week</h2>
-            <div className="homepage__stats">
-                <div className="homepage__stats__scroll">
-                    <div className="homepage__stats__card">
-                        <h3>Cards seen</h3>
-                        <span>10</span>
-                    </div>
-                    <div className="homepage__stats__card">
-                        <h3>Correct cards</h3>
-                        <span>6</span>
-                    </div>
-                    <div className="homepage__stats__card">
-                        <h3>Incorrect cards</h3>
-                        <span>4</span>
-                    </div>
-                </div>
-            </div>
-            <h2>Your statistics this month</h2>
-            <div className="homepage__stats">
-                <div className="homepage__stats__scroll">
-                    <div className="homepage__stats__card">
-                        <h3>Cards seen</h3>
-                        <span>10</span>
-                    </div>
-                    <div className="homepage__stats__card">
-                        <h3>Correct cards</h3>
-                        <span>6</span>
-                    </div>
-                    <div className="homepage__stats__card">
-                        <h3>Incorrect cards</h3>
-                        <span>4</span>
-                    </div>
-                </div>
-            </div>
-        </div>
+        // If the user is logged in
+        <DashboardHome />
     );
 }
 
